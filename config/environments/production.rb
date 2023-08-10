07268a6736f5d3263035f9921faf28e3b81baf6a
nagiriko <r.nagihashi@gmail.com>
2023-08-10 14:48:17 +0900

    クラウドストレージサービスを修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :amazon # または適切なクラウドストレージサービスを指定
+  config.active_storage.service = :local # または適切なクラウドストレージサービスを指定
   config.active_storage.queue = :inline
 
   # Mount Action Cable outside main process or domain.
