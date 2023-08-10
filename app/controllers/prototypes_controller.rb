7ea9fb06d2f8de0c4b8455622ee48b201d3571ee
nagiriko <r.nagihashi@gmail.com>
2023-08-10 16:34:24 +0900

    Issuesの修正
    ・ 空の入力欄がある場合は、編集できずにそのページに留まること
    ・ 正しく編集できた場合は、詳細ページへ遷移すること
    ・ログイン・ログアウトの状態に関わらず、各ページのユーザー名をクリックすると、ユーザーの詳細ページへ遷移すること
    ・ログイン・ログアウトの状態に関わらず、ユーザーの詳細ページには、そのユーザーの詳細情報（名前・プロフィール・所属・役職）と、そのユーザーが投稿したプロトタイプが表示されていること

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -40,9 +40,12 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    prototype.update(prototype_params)
-    redirect_to root_path
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to @prototype
+    else
+      render :edit
+    end
   end
 
   private
